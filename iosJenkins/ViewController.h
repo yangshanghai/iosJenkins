d629e0d0dc9d2de990c6c6d2351e4b02f3f6e7ef
Shanghai Yang <shanghai.yang2013@gmail.com>
2016-03-10 11:20:45 +0800

    first commit

---
+++ iosJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosJenkins
+//
+//  Created by  on 3/10/16.
+//  Copyright © 2016 shenzhenware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iosJenkins/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iosJenkins
+//
+//  Created by  on 3/10/16.
+//  Copyright © 2016 shenzhenware. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
