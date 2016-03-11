d81c64ff46b45875348b463e9e5df705a58f4728
Shanghai Yang <shanghai.yang2013@gmail.com>
2016-03-11 11:58:52 +0800

    test

--- iosJenkinsTests/iosJenkinsTests.m
+++ iosJenkinsTests/iosJenkinsTests.m
@@ -27,7 +27,7 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCTAssert(NO, @"Pass");
+    XCTAssert(YES, @"Pass");
 }
 
 - (void)testPerformanceExample {
